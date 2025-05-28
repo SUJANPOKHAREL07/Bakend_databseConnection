3dc869738696dfe4bbac1234a90deaa332b1c5fa
sujan07 <sujanpokharel704@gmail.com>
2025-05-28 21:00:09 +0545

    initial routing is done

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@
 import express from "express"
 import productRouter from "./router/productRouter"
 import categoryRouter from "./router/categoryRouter"
+import userRouter from "./router/userRouter"
 const app=express()
 app.use(express.json())
 const PORT=3000
 
 app.use("/product",productRouter)
 app.use("/category",categoryRouter)
+app.use("/user",userRouter)
 
 app.listen(PORT ,()=>{
     console.log("you are in server Port no:",PORT)
