3cb5ef338abd9b16bc8dc1565da70d2f78fde745
sujanpokharel07 <sujanpokharel704@gmail.com>
2025-06-12 11:58:36 +0545

    user login are stored in database

--- src/MongoDBModule/userCredentials/loginCredentials.ts
+++ src/MongoDBModule/userCredentials/loginCredentials.ts
@@ -1,11 +1,17 @@
 import userModal from "../usersModal/userMongoDB";
 import userLogin from "./loginMongoDB";
 
-
+async function storeLoginDetailsService(name:string,password:string){
+  const login=new userLogin({
+    name:name,
+    passwowrd:password
+  })
+  return await login.save()
+}
 
 async function checkUserget(name:string,password:string){
     return await userModal.find({name,password})
 }
 
 
-export  {checkUserget}
+export  {checkUserget,storeLoginDetailsService}
--- src/controller/LoginController/loginController.ts
+++ src/controller/LoginController/loginController.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from "express";
-import { checkUserget } from "../../MongoDBModule/userCredentials/loginCredentials";
+import { checkUserget, storeLoginDetailsService } from "../../MongoDBModule/userCredentials/loginCredentials";
 
 
 export const createLogin=async(req:Request,res:Response)=>{
   try{
-    const {name,password}=req.body
-  
     
+    const {name,password}=req.body
+ 
    const getLogindetails = await checkUserget(name, password);
    console.log(getLogindetails);
   
    if (getLogindetails && getLogindetails.length > 0) {
+    
+     const saveLogindata=await storeLoginDetailsService(name,password)
+    res.status(200).json(saveLogindata)
+  
+   
      res.status(200).json("Logged in");
    } else {
      res.status(401).json("Invalid credentials");
